2217122d02c78194b35221cc7a61537dce023829
idokahana <idoka1233@gmail.com>
2025-04-15 18:24:39 +0300

    frontend

--- backend/src/controllers/bank/validator.ts
+++ backend/src/controllers/bank/validator.ts
@@ -1,14 +1,14 @@
 import Joi from "joi";
 
-// export const newActionValidator = Joi.object({
-//   accountNumber: Joi.string().length(4).required(),
-//   type: Joi.string().required(),
-//   amount: Joi.string().min(0).required(),
-//   date: Joi.date().required(),
-//   interestRate: Joi.string().min(0).optional(),
-//   numOfPayments: Joi.string().min(1).optional(),
-//   loanDate: Joi.date().optional(),
-// });
+export const newActionValidator = Joi.object({
+  accountNumber: Joi.string().length(4).required(),
+  type: Joi.string().required(),
+  amount: Joi.number().min(0).required(),
+  date: Joi.date().required(),
+  interestRate: Joi.number().min(0).optional(),
+  numOfPayments: Joi.number().min(1).optional(),
+  loanDate: Joi.date().optional(),
+});
 
 export const newActionParamsValidator = Joi.object({
   accountNumber: Joi.string().length(4).required(),
